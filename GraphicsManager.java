7877f9dcbbf4720795b0fac1e6d94f02196d9be7
trrt-good <t.atwood.rose@gmail.com>
2022-04-20 12:55:56 -0700

    new models

--- GraphicsManager.java
+++ GraphicsManager.java
@@ -16,7 +16,7 @@ public class GraphicsManager
 
     public static void startGraphics(String name)
     {
-        gameObject1 = new GameObject(new Vector3(0, 0, 0), new EulerAngle(Math.toRadians(0), Math.toRadians(0), Math.toRadians(0)), 1, "rs1000.obj", new Color(50, 50, 50));
+        gameObject1 = new GameObject(new Vector3(0, 0, 0), new EulerAngle(Math.toRadians(0), Math.toRadians(0), Math.toRadians(0)), 100, "jet.obj", new Color(50, 50, 50));
         gameObject1.shading = true;
         // gameObject2 = new GameObject(new Vector3(0, 0, 0), "cat.obj", new Color(200, 200, 200), new EulerAngle(0, Math.toRadians(0), Math.toRadians(0)), 1, true);
         // gameObject2.shading = true;
@@ -38,7 +38,7 @@ public class GraphicsManager
         renderingPanel.setCamera(cam);
         //cam.setFreeControls(renderingPanel, 200, 10);
         cam.setOrbitControls(renderingPanel, gameObject1, 1000, 10);
-        renderingPanel.setLighting(new Lighting(new Vector3(1, -1, 1), 70, 50));
+        renderingPanel.setLighting(new Lighting(new Vector3(-1, -1, -1), 70, 500));
         renderingPanel.addGameObject(gameObject1);
         renderingPanel.setFog(1000, 3000, new Color(190, 210, 245));
         renderingPanel.startRenderUpdates();
