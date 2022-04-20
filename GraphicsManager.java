4010298bf90e0c24c47bacecdde413fb988c6e55
trrt-good <t.atwood.rose@gmail.com>
2022-04-20 16:53:02 -0700

    new models

--- GraphicsManager.java
+++ GraphicsManager.java
@@ -16,7 +16,7 @@ public class GraphicsManager
 
     public static void startGraphics(String name)
     {
-        gameObject1 = new GameObject(new Vector3(0, 0, 0), new Vector3(-100, 10, -100), new EulerAngle(Math.toRadians(0), Math.toRadians(90), Math.toRadians(90)), 1, "lowPolyCity3.obj", new Color(100, 100, 100));
+        gameObject1 = new GameObject(new Vector3(0, 0, 0), new Vector3(0, 10, 0), new EulerAngle(Math.toRadians(0), Math.toRadians(0), Math.toRadians(0)), 100, "f14.obj", new Color(100, 100, 100));
         gameObject1.backFaceCull = true;
         //gameObject2 = new GameObject(new Vector3(0, 0, 0), new EulerAngle(Math.toRadians(0), Math.toRadians(0), Math.toRadians(0)), 150, "jet.obj", new Color(50, 50, 50));
         System.out.println("Creating graphics... ");
