b6ccf79af3851251ccd84b8de9b627433ddb2040
trrt-good <t.atwood.rose@gmail.com>
2022-05-09 13:05:51 -0700

    speed increases

---
+++ Vertex.java
@@ -0,0 +1,13 @@
+public class Vertex 
+{
+    private Vector3 worldCoordinate;
+    private Vector2 screenCoordinate;
+    private double distanceToCamera;
+
+    public Vertex(Vector3 coordinate3d)
+    {
+        worldCoordinate = coordinate3d;
+    }
+
+    
+}
